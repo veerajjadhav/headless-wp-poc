fab760183b8ff4fc11d1f986fd30f5339c213611
Veeraj Jadhav <veerajjadhav1991@gmail.com>
2025-06-13 12:13:51 +0530

    Initial commit for headless WP POC

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-red-500 text-4xl font-bold">This should be red!!</div>
+   
+    </div>
+  );
+}
+
+export default App;
